9b6aa9397f3865c4a75ae7c2e60ff3e1c8347f5d
Matias Ribichich <guli4073@gmail.com>
2018-06-10 20:42:03 -0300

    Changed from xml parsing to string parsing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
 const {promisify} = require('util');
-const xml2js = require('xml2js');
+// const xml2js = require('xml2js');
 const {any, contains, keys, not, equals} = require('ramda');
 
 async function asyncForEach(array, callback) {
@@ -13,19 +13,19 @@ async function asyncForEach(array, callback) {
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
-const parseString = promisify(xml2js.parseString);
+// const parseString = promisify(xml2js.parseString);
 
-function anyRestorePackages(xml) {
-  return any(a => contains('RestorePackages', keys(a)), xml.Project.PropertyGroup);
-}
+// function anyRestorePackages(xml) {
+//   return any(a => contains('RestorePackages', keys(a)), xml.Project.PropertyGroup);
+// }
 
-function anyAutoGenerateBindingRedirects(xml) {
-  return any(a => contains('AutoGenerateBindingRedirects', keys(a)), xml.Project.PropertyGroup);
-}
+// function anyAutoGenerateBindingRedirects(xml) {
+//   return any(a => contains('AutoGenerateBindingRedirects', keys(a)), xml.Project.PropertyGroup);
+// }
 
-function anyTarget(xml) {
-  return xml.Project.Target && any(a => equals(a.$.Name, 'ForceGenerationOfBindingRedirects'), xml.Project.Target);
-}
+// function anyTarget(xml) {
+//   return xml.Project.Target && any(a => equals(a.$.Name, 'ForceGenerationOfBindingRedirects'), xml.Project.Target);
+// }
 
 (async () => {
   const targetXml = `<Target Name="ForceGenerationOfBindingRedirects" AfterTargets="ResolveAssemblyReferences" BeforeTargets="GenerateBindingRedirects" Condition="'$(AutoGenerateBindingRedirects)' == 'true'">
@@ -34,36 +34,62 @@ function anyTarget(xml) {
     <GenerateBindingRedirectsOutputType>true</GenerateBindingRedirectsOutputType>
   </PropertyGroup>
   </Target>`;
-  const targetObj = await parseString(targetXml);
+  // const targetObj = await parseString(targetXml);
 
   glob(path.join(process.argv[2], 'src/**/*.csproj'), null, async (er, files) => {
     await asyncForEach(files, async file => {
       console.log(file);
 
-      const content = await readFile(file);
+      const content = await readFile(file, 'utf8');
 
-      const xml = await parseString(content);
+      // const xml = await parseString(content);
 
-      // RestorePackages: [ 'true' ],
-      // RestoreProjectStyle: [ 'PackageReference' ],
-      // AutoGenerateBindingRedirects: [ 'true' ]
+      // if (not(anyRestorePackages(xml))) {
+      //   xml.Project.PropertyGroup[0] = {...xml.Project.PropertyGroup[0], RestorePackages: ['true']};
+      // }
 
-      if (not(anyRestorePackages(xml))) {
-        xml.Project.PropertyGroup[0] = {...xml.Project.PropertyGroup[0], RestorePackages: ['true']};
+      // if (not(anyAutoGenerateBindingRedirects(xml))) {
+      //   xml.Project.PropertyGroup[0] = {...xml.Project.PropertyGroup[0], AutoGenerateBindingRedirects: ['true']};
+      // }
+
+      // if (not(anyTarget(xml))) {
+      //   xml.Project = {...xml.Project, ...targetObj};
+      // }
+
+      // const builder = new xml2js.Builder();
+      // const xmlToSave = builder.buildObject(xml);
+
+      // await writeFile(file, xmlToSave);
+
+      let lines = content.split('\n');
+
+      const firstPropertyGroupEndIndex = lines.findIndex(f => f.trim() === '</PropertyGroup>');
+
+      if (not(any(a => contains('ForceGenerationOfBindingRedirects', a), lines))) {
+        lines = [
+          ...lines.slice(0, firstPropertyGroupEndIndex + 1),
+          targetXml,
+          ...lines.slice(firstPropertyGroupEndIndex + 1),
+        ];
       }
 
-      if (not(anyAutoGenerateBindingRedirects(xml))) {
-        xml.Project.PropertyGroup[0] = {...xml.Project.PropertyGroup[0], AutoGenerateBindingRedirects: ['true']};
+      let linesToInclude = [];
+
+      if (not(any(a => contains('<RestorePackages>', a), lines))) {
+        linesToInclude = [...linesToInclude, '<RestorePackages>true</RestorePackages>'];
       }
 
-      if (not(anyTarget(xml))) {
-        xml.Project = {...xml.Project, ...targetObj};
+      if (not(any(a => contains('<AutoGenerateBindingRedirects>', a), lines))) {
+        linesToInclude = [...linesToInclude, '<AutoGenerateBindingRedirects>true</AutoGenerateBindingRedirects>'];
       }
 
-      const builder = new xml2js.Builder();
-      const xmlToSave = builder.buildObject(xml);
+      lines = [
+        ...lines.slice(0, firstPropertyGroupEndIndex),
+        ...linesToInclude,
+        ...lines.slice(firstPropertyGroupEndIndex),
+      ];
 
-      await writeFile(file, xmlToSave);
+      await writeFile(file, lines.join('\n'));
     });
   });
 })();
